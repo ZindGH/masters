edaf3a62bdf9dfe7cfb68c2e620aeb648c994113
ZindGH <zzzRRzzz@mail.ru>
2021-04-13 00:39:05 +0300

    STA 1.3.4

--- R-peak detection.py
+++ R-peak detection.py
@@ -1,7 +1,25 @@
 import numpy as np
+from Processing import FS
+import plotly.graph_objects as go
 
 
-def matched_filters():
-    """Creates fetal QRS and PQRST templates from [22]"""
-    qrs = 
-    return qrs_template, pqrst_template
+def filter_templates(fs: int):
+    """Creates fetal QRS and PQRST templates from [22]
+    qrs: h' = -0.007^2 * t * e^(-t^2/0.007^2)"""
+    N = int(0.04 * fs + 1)
+    qrs = np.zeros((N, 2))
+    qrs[:, 1] = np.arange(-0.02, 0.02 + 1 / fs, 1 / fs)  # Time
+    qrs[:, 0] = -2 * np.exp(-np.power(qrs[:, 1], 2) / np.power(0.007, 2)) * qrs[:, 1] / (np.power(0.007, 2))
+    qrs[:, 0] = (qrs[:, 0] - qrs[:, 0].mean()) / (qrs[:, 0].std())
+    #########
+    pqrst = None
+    print(qrs)
+    return qrs, pqrst
+
+
+templ, _ = filter_templates(FS)
+
+# Plot template
+# fig = go.Figure()
+# fig.add_trace(go.Scatter(x=templ[:, 1], y=templ[:, 0]))
+# fig.show()
