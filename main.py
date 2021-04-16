28c43240f6a6dfd7e6dff0fd9968c07a5172ea43
ZindGH <zzzRRzzz@mail.ru>
2021-04-16 21:27:34 +0300

    STA 1.4.1

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import analysis
 if __name__ == '__main__':
     # data_extraction.edf2npy_save()
     data, _, qrs = processing.open_record(qrs=True)
-    # data = Processing.notch_filter(data, 60)
-    # data = Processing.bandpass_filter(data, 100, 0.05)
-    # Processing.FS = 25
-    # Processing.plot_record(data, qrs=qrs, time_range=(0, 0.02))
+    # data = processing.notch_filter(data, 60)
+    # data = processing.bandpass_filter(data, 100, 0.05)
+    # processing.FS = 25
+    processing.plot_record(data, qrs=qrs, time_range=(0, 0.02))
     qrs_fir, _ = analysis.filter_templates(processing.FS)
     f_data = processing.matched_filter(data, qrs_fir[:, 1])
     print(f_data)
